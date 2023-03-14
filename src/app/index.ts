99c97da789d7414904143dc64e86c99ddc144a88
moneyinto <woyaoqianjin@gmail.com>
2023-03-14 14:44:23 +0800

    处理系统没有ffmpeg的情况

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,22 +1,23 @@
 import express from "express";
-import ffmpeg from "ffmpeg";
+// import ffmpeg from "ffmpeg";
 import Busboy from "busboy";
 import fs from "fs";
 import path from "path";
+import ffmpegPath from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from "fluent-ffmpeg";
+
+ffmpeg.setFfmpegPath(ffmpegPath.path);
 
 const app = express();
 
-app.all("*", function (req, res, next) {
+app.all("*", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Methods",
         "PUT, GET, POST, DELETE, OPTIONS"
     );
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", [
-        "Content-Type",
-        "form-data"
-    ]);
+    res.header("Access-Control-Allow-Headers", ["Content-Type", "form-data"]);
     next();
 });
 
@@ -25,22 +26,28 @@ app.use(express.static(path.join(__dirname, "./assets")));
 const convertVideoH264 = (filePath: string, outputPath: string) => {
     return new Promise((resolve, reject) => {
         try {
-            const process = new ffmpeg(filePath);
-            process.then(
-                (video) => {
-                    video.setVideoCodec("h264").save(outputPath, (err) => {
-                        if (err) {
-                            reject("Error: " + err);
-                        } else {
-                            resolve(true);
-                        }
-                    });
-                },
-                (err) => {
-                    console.log("Error: " + err);
-                    reject(err);
-                }
-            );
+            // const process = ffmpeg(filePath);
+            ffmpeg(filePath)
+                .videoCodec("libx264")
+                .on("end", () => {
+                    resolve(true);
+                })
+                .save(outputPath);
+            // process.then(
+            //     (video) => {
+            //         video.setVideoCodec("h264").save(outputPath, (err) => {
+            //             if (err) {
+            //                 reject("Error: " + err);
+            //             } else {
+            //                 resolve(true);
+            //             }
+            //         });
+            //     },
+            //     (err) => {
+            //         console.log("Error: " + err);
+            //         reject(err);
+            //     }
+            // );
         } catch (err) {
             console.log("Error: " + err);
             reject(err);
@@ -65,8 +72,7 @@ app.get("/api/tranformVideoH264", (req, res) => {
         success: false,
         msg: "只支持mp4文件的文件转码"
     });
-})
-
+});
 
 app.post("/api/tranformVideoH264", (req, res) => {
     const name = createRandomCode(6) + new Date().getTime();
@@ -92,7 +98,10 @@ app.post("/api/tranformVideoH264", (req, res) => {
     busboy.on("finish", async () => {
         if (saveTo) {
             try {
-                convertVideoH264(saveTo, path.resolve(__dirname, `./assets/output/${name}.mp4`)).then(isFinish => {
+                convertVideoH264(
+                    saveTo,
+                    path.resolve(__dirname, `./assets/output/${name}.mp4`)
+                ).then((isFinish) => {
                     if (isFinish) {
                         res.send({
                             success: true,
